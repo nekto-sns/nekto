13ee4bf419e7530b48e6c0fd98fefe6d071dd95b
Itsuki0222 <175463719+Itsuki0222@users.noreply.github.com>
2026-07-26 11:39:59 +0900

    fix: Echo本体が投げるエラーを拾えてなかったので修正

--- app/shared/errorhandler/apperr.go
+++ app/shared/errorhandler/apperr.go
@@ -15,7 +15,7 @@ type errorResponse struct{
 func ErrorHandler(c *echo.Context, err error) {
 	var appErr *echo.HTTPError
 	if errors.As(err, &appErr) {
-		if internalErr := errors.Unwrap(err); internalErr != nil {
+		if internalErr := errors.Unwrap(err); internalErr != nil && appErr.Code != http.StatusNotFound {
 			slog.Error("Request processing failed", "err", internalErr)
 		}
 		_ = c.JSON(appErr.Code, errorResponse{
@@ -23,6 +23,21 @@ func ErrorHandler(c *echo.Context, err error) {
 		})
 		return
 	}
+
+	var sc echo.HTTPStatusCoder
+	if errors.As(err, &sc) {
+		if code := sc.StatusCode(); code != 0 {
+			if code != http.StatusNotFound {
+				slog.Warn("HTTP error", "err", err.Error())
+			}
+			_ = c.JSON(code, errorResponse{
+				Message: err.Error(),
+			})
+			return
+		}
+	}
+
+
 	_ = c.JSON(http.StatusInternalServerError, errorResponse{
 		Message: "Internal server error",
 	})
